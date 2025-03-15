cf169171e4fbca599aa1f112db7fd3312d43b0ca
300plus <info@300pl.us>
2025-03-15 08:51:49 +0000

    Version 1.0.29: Reverted from 1.1.0 for update compatibility

--- herochat.php
+++ herochat.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HeroChat
  * Description: HeroChat allows you to display a customizable AI chatbot on selected pages of your website.
- * Version: 1.1.0
+ * Version: 1.0.29
  * Author: HeroChat
  * Author URI: https://herochat.org/plugin
  * License: GPL2
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-define('HEROCHAT_VERSION', '1.1.0');
+define('HEROCHAT_VERSION', '1.0.29');
 
 // Include Composer autoloader for Plugin Update Checker
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
